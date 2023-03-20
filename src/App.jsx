1413f232228bea1e3f6505f11430ca05dab97813
IsmaelHK1 <ismael@Ordinateur>
2023-03-20 11:37:29 +0100

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div style={{verticalAlign : 'middle', display : 'inline-block'}}>
+
       <Counter1 count = {count } setCount = { setCount} />
       <Counter2 count2 = {count2} setCount2 = {setCount2} />
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
