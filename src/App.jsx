d1eb1e54b6a49a848022b83260b75e4f9952a304
IsmaelHK1 <ismael@Ordinateur>
2023-03-20 12:44:40 +0100

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [count2, setCount2] = useState(0)
 
 
   return (
@@ -21,8 +19,8 @@ function App() {
       <h1>Vite + React</h1>
       <div style={{display : 'flex'}}>
 
-      <Counter1 count = {count } setCount = { setCount} />
-      <Counter2 count2 = {count2} setCount2 = {setCount2} />
+      <Counter1  />
+      <Counter1  />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
@@ -30,30 +28,19 @@ function App() {
     </div>
   )
 }
-function Counter1(props){
+function Counter1(){
+  const [count, setCount] = useState(0)
+
   return (
     <div className="card">
         <h3>COUNT 1 </h3>
         <p>
-        {props.count}
+        {count}
         </p>
-        <button onClick={() => props.setCount(props.count + 1)}>
+        <button onClick={() => setCount(count + 1)}>
           Counter ++
         </button>
       </div>
-  )
-}
-function Counter2(props) {
-  return ( 
-    <div className="card">
-    <h3>COUNT 2 </h3>
-    <p>
-    {props.count2}
-    </p>
-    <button onClick={() => props.setCount2(props.count2 + 1)}>
-      Counter ++
-    </button>
-  </div>
   )
 }
 
